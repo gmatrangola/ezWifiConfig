434458f85cd8b02f7732d5cc6523d9aa5495e584
Geoff Matrangola <geoff@matrangola.com>
2019-11-04 20:59:05 -0500

    Setup proper polkit policies
    * avoids the not authorized to modify networking error

--- ezWifiConfigLinux/src/main/scripts/installService.sh
+++ ezWifiConfigLinux/src/main/scripts/installService.sh
@@ -2,5 +2,10 @@
 # Create the user, group that can't log in but has access to Bluetooth
 echo Installing ezWifiConfig Service
 sudo cp /opt/ezWifiConfig/scripts/ezWifiConfig.service /etc/systemd/system/
+sudo mkdir -p /var/lib/polkit-1/localauthority/30-com.electrazoom.d
+sudo cp /opt/ezWifiConfig/scripts/com.electrazoom.ezwificonfig.pkla /var/lib/polkit-1/localauthority/30-com.electrazoom.d
+sudo mv /var/lib/polkit-1/localauthority/10-vendor.d/org.freedesktop.NetworkManager.pkla /var/lib/polkit-1/localauthority/10-vendor.d/org.freedesktop.NetworkManager.pkla.bak
 sudo systemctl daemon-reload
+sudo systemctl restart polkit
 sudo systemctl enable ezWifiConfig.service
+sudo systemctl start ezWifiConfig.service
--- ezWifiConfigLinux/src/main/scripts/preInstall.sh
+++ ezWifiConfigLinux/src/main/scripts/preInstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# echo Creating ezwificonfig user
+# Create the user, group that can't log in but has access to Bluetooth and network config permissions
 sudo useradd -r -s /bin/nologin ezwificonfig
 sudo usermod -aG bluetooth ezwificonfig
+sudo usermod -aG netdev ezwificonfig
